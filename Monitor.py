f50b309eb49ed082788b8ace985cd15b957257b1
Milad Barooni <35670365+miladbarooni@users.noreply.github.com>
2019-07-05 19:30:02 +0430

    PythonMonitor

---
+++ Monitor.py
@@ -0,0 +1,52 @@
+from tkinter import *
+# import random
+import time
+
+
+# first to make memory list
+filename = "Memory.txt"
+memory = []
+file = open(filename, "r")
+file_str = file.read()
+file_str = file_str.replace(" ", "")
+# print (file_str)
+file.close()
+file_arr = file_str.split("-")
+list_of_dics = []
+for moment in file_arr:
+    file_dic = {}
+    moment_arr = moment.split(",")
+    for value in moment_arr:
+        value_arr = value.split(":")
+        if len(value_arr) == 1:
+            continue
+        file_dic[value_arr[0]] = value_arr[1]
+#     print (file_dic)
+    memory.append(file_dic)
+
+print (memory)
+
+
+root = Tk()
+labels = []
+# memory = []
+
+# now time for tkinter
+print (len(memory))
+for i in range(4):
+    for j in range(4):
+        label = Label(root, text = "test")
+        label.grid(row=i, column = j, padx = 10, pady= 10)
+        labels.append(label)
+for k in range(len(memory)):
+    time.sleep(0.01)
+    for i in range(4):
+        for j in range(4):
+            if (int(memory[k]["Memo"+str(4*i+j+1)]) == 0):
+                labels[4*i+j].config(text = "")
+            else:
+                labels[4*i+j].config(text = chr(int(memory[k]["Memo"+str(4*i+j+1)])))
+    root.update()
+
+root.mainloop()
+# root.destroy()
